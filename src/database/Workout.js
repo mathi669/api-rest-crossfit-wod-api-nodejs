52bd9cab44c0ed73d52a7432f80772cb470f34b5
mathi669 <matias.luarte@hotmail.com>
2022-07-25 18:44:11 -0400

    logica POST/create

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -1,7 +1,21 @@
 const DB = require("./db.json");
+const { saveToDatabase } = require("./utils");
 
 const getAllWorkouts = () => {
-    return DB.workouts;
-}
+  return DB.workouts;
+};
 
-module.exports = { getAllWorkouts }
+const createNewWorkout = (newWorkout) => {
+  const isAlreadyAdded = DB.workouts.findIndex(
+    (workout) => workout.name === newWorkout.name
+  );
+
+  if(isAlreadyAdded) {
+    return;
+  }
+
+  DB.workouts.push(newWorkout);
+  saveToDatabase(DB)
+};
+
+module.exports = { getAllWorkouts, createNewWorkout};
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const v1WorkoutRouter = require('./v1/routes/workoutRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+app.use(express.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {console.log(`🚀 Server listening on port ${PORT}`) });
