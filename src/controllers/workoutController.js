727102eaa7efbe9accd1dc8a80f845e82f1d90ab
mathi669 <matias.luarte@hotmail.com>
2022-07-25 15:26:51 -0400

    servicio y modelo de acceso a datos

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,23 +1,28 @@
 const workoutService = require("../services/workoutService");
 
 const getAllWorkouts = (req, res) => {
+    const allWorkouts = workoutService.getAllWorkouts();
     res.send("Get all workouts");
 };
 
 const getOneWorkout = (req, res) => {
+    const workout = workoutService.getOneWorkout(req.params.workoutId);
     res.send(`Get workout ${req.params.workoutId}`);
 };
 
 const createNewWorkout = (req, res) => {
+    const createdWorkout = workoutService.createNewWorkout(req.params.workoutId);
     res.send(`Create workout ${req.params.workoutId}`);
 };
 
 const updateOneWorkout = (req, res) => {
+    const updatedWorkout = workoutService.updateOneWorkout(req.params.workoutId);
     res.send(`Update workout ${req.params.workoutId}`);
 
 };
 
 const deleteOneWorkout = (req, res) => {
+    //workoutService.deleteOneWorkout(req.params.workoutId);
     res.send(`Delete workout ${req.params.workoutId}`);
 };
 
